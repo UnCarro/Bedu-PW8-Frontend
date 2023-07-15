c1973ff9fd03d89a6f3b3f2e0af1e64e54f6b099
Marco Cardenas <bcv10g@hotmail.com>
2023-07-15 13:11:17 -0600

    PW8: button testing

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -2,18 +2,29 @@ import axios from "axios"
 
 export function MyForm() {
 
-    const handleSubmit = () => {
-        axios.post("",
-            {})
-            .then()
-            .catch();
+    const handleSubmit = (event) => {
+        console.log(event);
+        // axios.post("/test",
+        //     { a: event.target.value })
+        //     .then(response => {
+        //         console.log(response);
+        //     }
+        //     )
+        //     .catch(
+        //         e => {
+        //             console.log(e);
+        //         }
+        //     );
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Nombre de entrevistador:
-                <input id="interviewerName" type="text" />
-            </label>
-        </form>
+        <>
+            <form >
+                <label>Nombre de entrevistador:
+                    <input id="interviewerName" type="text" />
+                </label>
+            </form>
+            <button onClick={handleSubmit}>Submit</button>
+        </>
     )
 }
