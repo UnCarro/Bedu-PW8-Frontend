fb88eae8cdaa0bf1aa8483dad6c4ce685f9b3ae0
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2023-07-15 16:51:12 -0600

    PW8: Wrapper y funcionalidad para el resto de inputs

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -4,28 +4,51 @@ import { useState } from "react";
 export function MyForm() {
 
 
-    const [interviewerName, setInterviewerName] = useState({});
+    const [interviewerName, setInterviewerName] = useState("");
+    const [interviewerLastName, setInterviewerLastName] = useState("");
+    const [interviewerEmail, setInterviewerEmail] = useState("");
 
-    const handleOnChange = (event) => {
+    const interviewerWrapper = {
+        name: "",
+        lastName: "",
+        email: ""
+    }
+
+    const handleOnChangeName = (event) => {
         setInterviewerName(
-            {
-                Name: event.target.value,
-                lastName: "",
-                email: "",
-                isActive: ""
-            }
+            event.target.value
         )
 
         console.log(interviewerName);
     }
 
+    const handleOnChangeLastName = (event) => {
+        setInterviewerLastName(
+            event.target.value
+        )
+
+        console.log(interviewerLastName);
+    }
+
+
+    const handleOnChangeEmail = (event) => {
+        setInterviewerEmail(
+            event.target.value
+        )
+
+        console.log(interviewerEmail);
+    }
+
     const handleSubmit = () => {
 
-        console.log(interviewerName);
-        axios.post("http://localhost:8080/add-interviewer",
-            {
-                interviewerName
-            })
+        interviewerWrapper.name = interviewerName;
+        interviewerWrapper.lastName = interviewerLastName;
+        interviewerWrapper.email = interviewerEmail;
+
+
+        console.log(interviewerWrapper);
+        axios.post("http://localhost:8080/add-interviewer", { interviewerWrapper }
+        )
             .then(response => {
                 console.log(response);
             }
@@ -40,9 +63,16 @@ export function MyForm() {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <label>Nombre de entrevistador:
-                    <input id="interviewerName" onChange={handleOnChange} type="text" placeholder="enter interviewer name" />
+                <label>Nombre del entrevistador:
+                    <input id="interviewerName" onChange={handleOnChangeName} type="text" placeholder="enter interviewer name" />
                 </label>
+                <label>Apellido del entrevistador:
+                    <input id="interviewerLastName" onChange={handleOnChangeLastName} type="text" placeholder="enter interviewer Last name" />
+                </label>
+                <label>Email del entrevistador:
+                    <input id="interviewerEmail" onChange={handleOnChangeEmail} type="text" placeholder="enter interviewer Email" />
+                </label>
+
 
             </form>
             <button onClick={handleSubmit}>Submit</button>
