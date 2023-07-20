a5f7607d32e0dca8a5536c55d632db98dbdb4233
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2023-07-19 20:42:11 -0600

    PW8: Testing Starter

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { esmodules: true } }],
     ["@babel/preset-react", { runtime: "automatic" }],
---
+++ tests/demo.test.js
@@ -0,0 +1,3 @@
+describe("Probando", () => {
+  test("should", () => {});
+});
